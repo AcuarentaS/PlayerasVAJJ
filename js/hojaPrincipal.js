4de97972ee549fe9f9cda70092f9b98166962da8
AcuarentaS <acuarentas1300@gmail.com>
2025-01-28 18:57:32 -0600

    Delete js directory

--- js/hojaPrincipal.js
+++
@@ -1,6 +0,0 @@
-const modal = document.getElementById('modalContainer');
-const btnCloseModal = document.getElementById('btnCloseModal');
-
-btnCloseModal.addEventListener('click', () => {
-    modal.removeAttribute('open');
-});
